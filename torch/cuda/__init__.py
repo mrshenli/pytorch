0a774e210edf513975e10aa4be054d71c8233707
Shen Li <shenli@fb.com>
2019-01-06 11:20:47 -0800

    fix initialization error

--- torch/cuda/__init__.py
+++ torch/cuda/__init__.py
@@ -473,7 +473,7 @@ def _dummy_type(name):
         class_name = self.__class__.__name__
         raise RuntimeError(
             "Tried to instantiate dummy base class {}".format(class_name))
-    return type(storage_name, (object,), {"__init__": init_err})
+    return type(name, (object,), {"__init__": init_err})
 
 
 if not hasattr(torch._C, 'CudaDoubleStorageBase'):
@@ -485,6 +485,7 @@ if not hasattr(torch._C, 'CudaDoubleStorageBase'):
         torch._C.__dict__[storage_name] = _dummy_type(storage_name)
         torch._C.__dict__[tensor_name] = _dummy_type(tensor_name)
 
+if not hasattr(torch._C, '_CudaStreamBase'):
     torch._C.__dict__['_CudaStreamBase'] = _dummy_type('CudaStreamBase')
 
 
